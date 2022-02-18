cfafe9fd2a38cfedcf4c40b99e985d5013ae4169
Brian Chang <brianc@fobi.ai>
2022-02-17 20:18:05 -0800

    validate Article.body exists

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
+  # TODO: if there's a draft feature, can consider removing this body presence as the user would have a way to not publish articles
+  validates :body, presence: true
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,53 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'is valid with a title and body' do
-    article = Article.new
-    article.title = Faker::Name.name
-    article.body = Faker::Lorem.paragraph
+  context 'is valid' do
+    it 'with a title and a body' do
+      article = Article.new
+      article.title = Faker::Name.name
+      article.body = Faker::Lorem.paragraph
 
-    expect(article).to be_valid
+      expect(article).to be_valid
+    end
   end
 
-  it 'is valid with a title' do
-    article = Article.new
-    article.title = Faker::Name.name
+  context 'is not valid' do
+    it 'without a title and body' do
+      article = Article.new
 
-    expect(article).to be_valid
-  end
+      expect(article).to_not be_valid
+    end
 
+    it 'without a title' do
+      article = Article.new
+      article.body = Faker::Lorem.paragraph
 
-  it 'is not valid without a title' do
-    article = Article.new
+      expect(article).to_not be_valid
+    end
 
-    expect(article).to_not be_valid
-  end
+    it 'without a body' do
+      article = Article.new
+      article.title = Faker::Name.name
 
-  it 'is valid with a unique title' do
-    article_1 = Article.new
-    article_2 = Article.new
-
-    article_1.title = 'test 1'
-    article_2.title = 'test 2'
-    article_1.save
-    article_2.save
-
-    expect(article_1).to be_valid
-    expect(article_2).to be_valid
-    expect(Article.count).to eq(2)
-  end
-
-  it 'is not valid if title is not unique' do
-    article_1 = Article.new
-    article_2 = Article.new
-
-    article_1.title = 'test 1'
-    article_2.title = 'test 1'
-    article_1.save
-    article_2.save
-
-    expect(article_1).to be_valid
-    expect(article_2).to_not be_valid
-    expect(Article.count).to eq(1)
+      expect(article).to_not be_valid
+    end
   end
 end
